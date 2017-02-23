d5d1d62cac2a244f534410bd1635b2a2b5822213
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2017-02-23 19:05:02 +0100

    getroot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { ReportslistComponent } from './reportslist/reportslist.component';
         ReportslistComponent
     ],
     imports: [
-        MarkdownModule.getRoot(),
+        MarkdownModule,
         BrowserModule,
         FormsModule,
         HttpModule,
